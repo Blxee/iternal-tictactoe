27c8a80a7e117fe237f62b745cf06649e0183b13
Blxee <aminedev57@gmail.com>
2024-06-03 06:25:41 +0100

    Added: now there can be only 6 marks at a time

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,12 +1,17 @@
 extends Control
 
 var turn: Square.Mark = [Square.Mark.X, Square.Mark.O].pick_random()
+var stack = []
 
 
 func _on_mark_pressed(square: Square, index: int) -> void:
 	if not square.is_marked():
 		square.mark = turn
 		_swap_turn()
+		stack.append(square)
+		if stack.size() > 6:
+			var front: Square = stack.pop_front()
+			front.mark = Square.Mark.NONE
 
 
 func _swap_turn() -> void:
