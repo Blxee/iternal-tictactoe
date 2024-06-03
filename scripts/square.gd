6d1ecb1be9c11a9b25d1019cee476dac13251a82
Blxee <aminedev57@gmail.com>
2024-06-03 05:26:43 +0100

    Added: square scene with a tool script

--- scripts/square.gd
+++ scripts/square.gd
@@ -1,16 +1,28 @@
 @tool
+class_name Square
 extends Button
 
-signal mark_pressed(index: int)
+signal mark_pressed(spuare: Square, index: int)
 
+@export_enum('NONE', 'X', 'O') var mark: int = Mark.NONE:
+	set(value):
+		match value:
+			Mark.X: icon = x_texture
+			Mark.O: icon = o_texture
+			Mark.NONE: icon = null
+		mark = value
 
+@export_group('Textures')
 @export var x_texture: Texture = preload('res://assets/images/tictac_x.svg')
 @export var o_texture: Texture = preload('res://assets/images/tictac_o.svg')
 
+enum Mark { NONE, X, O }
 
-func _ready() -> void:
-	icon = x_texture
+
+func is_marked() -> bool:
+	return mark != Mark.NONE
 
 
 func _on_pressed() -> void:
-	mark_pressed.emit(0)
+	var index: int = get_parent().get_children().find(self)
+	mark_pressed.emit(self, index)
